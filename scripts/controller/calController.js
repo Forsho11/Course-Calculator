2a6ac3b1071e20cba9c7b40bb5ad9f836380fce6
Matheus <matheus200211@gmail.com>
2023-02-22 20:02:40 -0300

    Ajuste dos evento Click

--- scripts/controller/calController.js
+++ scripts/controller/calController.js
@@ -8,13 +8,44 @@ class CalController {
         this._timeEL = document.querySelector("#hora");
         this.currentDate;
         this.initialize();
+        this.initButtonsEvents();
     }
 
     initialize(){
+
+        this.setDisplayDateTime()
+
         setInterval(()=>{
-            this.displayDate = this.currentDate.toLocaleDateString(this._locale);
-            this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
+            this.setDisplayDateTime();
+
         }, 1000);
+    }
+
+    initButtonsEvents(){
+
+        let buttons = document.querySelectorAll("#buttons > g, #parts > g");
+
+        buttons.forEach( btn => {
+
+            btn.addEventListener('click', e => {
+            
+                console.log(btn.className.baseVal.replace("btn-",""));
+            
+            });
+
+        })
+    }
+
+    setDisplayDateTime(){
+
+        this.displayDate = this.currentDate.toLocaleDateString(this._locale, {
+            day: "2-digit",
+            month: "long",
+            year: "numeric"
+        });
+        this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
+
     }
 
     get displayTime(){
