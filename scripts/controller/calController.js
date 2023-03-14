72e797b66d4db9c9b8e664656d76b23d83649dd7
Matheus <matheus200211@gmail.com>
2023-03-14 12:04:57 -0300

    Ajuste de bugs ao incluir o ponto e adição de eventos nos botões do teclado

--- scripts/controller/calController.js
+++ scripts/controller/calController.js
@@ -13,6 +13,7 @@ class CalController {
         this._currentDate;
         this.initialize();
         this.initButtonsEvents();
+        this.initKeyboard();
 
     }
 
@@ -27,6 +28,57 @@ class CalController {
         }, 1000);
 
         this.setLastNumberToDisplay();
+    }
+
+    initKeyboard() {
+
+        document.addEventListener('keyup', e=> {
+
+            switch (e.key) {
+
+                case 'Escape':
+                    this.clearAll();
+                    break;
+    
+                case 'Backspace':
+                    this.clearEntry();
+                    break;
+    
+                case '+':    
+                case '-':    
+                case '*':    
+                case '/':    
+                case '%':
+                    this.addOperation(e.key);
+                    break;
+    
+                case 'Enter':
+                case '=':
+                    this.calc();
+                    break;
+    
+                case '.':
+                case ',':
+                    this.addDot();
+                    break;
+    
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(e.key));
+                    break;
+    
+            }
+
+        })
+
     }
 
     addEventListenerAll(element, events, fn){
@@ -106,9 +158,6 @@ class CalController {
             this._lastNumber = this.getLastItem(false);
 
         }
-
-        console.log(this._lastOperator);
-        console.log(this._lastNumber)
 
         let Result = this.getResult();
 
@@ -191,7 +240,7 @@ class CalController {
 
                 let newValue = this.getLastOperation().toString() + value.toString();
 
-                this.setLastOperation(parseFloat(newValue));
+                this.setLastOperation(newValue);
 
                 this.setLastNumberToDisplay();
 
@@ -209,6 +258,8 @@ class CalController {
     addDot(){
 
         let lastOperation = this.getLastOperation();
+
+        if (typeof lastOperation === 'string' && lastOperation.split('').indexOf('.') > -1) return;
 
         if (this.isOperator(lastOperation) || !lastOperation) {
 
